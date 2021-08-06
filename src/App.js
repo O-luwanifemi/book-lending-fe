5fa326146109b846541b4296dae4327baf998cc6
O-luwanifemi <66732138+O-luwanifemi@users.noreply.github.com>
2021-08-06 19:52:37 +0100

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <h1>Hello world</h1>
+  );
+};
+
+export default App;
