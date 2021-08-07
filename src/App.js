011214bda83c359c77e919faf028590c5b77b153
O-luwanifemi <66732138+O-luwanifemi@users.noreply.github.com>
2021-08-07 10:34:57 +0100

    Added user profile page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import UserProfile from "./pages/sidebar/UserProfile";
+
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <UserProfile />
   );
 };
 
