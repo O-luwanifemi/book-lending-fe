fc9621416a37860a73c153bf600cd61579a6e6a5
Maristina-Hub <maristyna@yahoo.com>
2021-08-08 18:57:18 +0100

    book component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import UserProfile from "./pages/sidebar/UserProfile";
-
+// import UserProfile from "./pages/sidebar/UserProfile";
+import BookList from '../src/components/booklistComponent.js'
 const App = () => {
   return (
-    <UserProfile />
+    <>
+    {/* <UserProfile /> */}
+    < BookList />
+    </>
   );
 };
 
