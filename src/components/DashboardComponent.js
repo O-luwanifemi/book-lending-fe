6a53d55ef5163b728a2d9e7b31408609864d6fbe
Maristina-Hub <maristyna@yahoo.com>
2021-08-18 12:00:11 +0100

    conflict resolution

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -1,6 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 import HistoryComponent from "./HistoryComponent";
 import HeaderComponent from "./layouts/HeaderComponent";
 import ShelfComponent from "./ShelfComponent";
@@ -16,10 +15,10 @@ const DashboardComponent = () => {
                     <Col className="col-md-6 my-2">
                         <h3>Books on Shelf</h3>
                         <ShelfComponent />
-
                         <hr/>
                         <Link to="/shelf">view all books on shelf</Link>
                     </Col>
+
                     <Col className="col-md-6 my-2">
                         <h3>Books Borrow History</h3>
                         <HistoryComponent />
@@ -27,6 +26,7 @@ const DashboardComponent = () => {
                         <hr/>
                         <Link to="/history">view all history</Link>
                     </Col>
+                    
                     <Col className="col-md-6 my-2">
 
                         <h3>Create A New Book</h3>
